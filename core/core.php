39919ca1a3b0696186cc04e1063bb7f6c2cb632f
Papp Krisztián <fejlesztes@letscode.hu>
2019-01-11 22:54:59 +0100

    php 20

--- core/core.php
+++ core/core.php
@@ -6,6 +6,10 @@ $page = $_GET["page"] ?? 1;
 $possiblePageSizes = [10, 25, 30, 40, 50];
 
 $connection = mysqli_connect($config['db_host'], $config['db_user'], $config['db_pass'], $config['db_name']);
+if (!$connection) {
+    logMessage('ERROR',"Connection error: ". mysqli_connect_error());
+    errorPage();
+} 
 
 $total = getTotal($connection);
 $offset = ($page - 1) * $size;
@@ -13,17 +17,36 @@ $offset = ($page - 1) * $size;
 $content = getPhotosPaginated($connection, $size, $offset);
 
 function getPhotosPaginated($connection, $size, $offset) {
-    $statement = mysqli_prepare($connection, 'SELECT * FROM photos LIMIT ? OFFSET ?');
-    mysqli_stmt_bind_param($statement, "ii", $size, $offset);
-    mysqli_stmt_execute($statement);
-    $result = mysqli_stmt_get_result($statement);
-    return mysqli_fetch_all($result, MYSQLI_ASSOC);
+    if ($statement = mysqli_prepare($connection, 'SELECT * FROM photos LIMIT ? OFFSET ?')) {
+        mysqli_stmt_bind_param($statement, "ii", $size, $offset);
+        mysqli_stmt_execute($statement);
+        $result = mysqli_stmt_get_result($statement);
+        return mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } else {
+        logMessage('ERROR','Query error: '. mysqli_error($connection));
+        errorPage();
+    }
+}
+
+function logMessage($level, $message) {
+    $file = fopen('application.log', "a");
+    fwrite($file, "[$level] $message". PHP_EOL);
+    fclose($file);
+}
+
+function errorPage() {
+    include "templates/error.php";
+    die();
 }
 
 function getTotal($connection) {
-    $result = mysqli_query($connection, "SELECT count(*) as count FROM photos");
-    $row = mysqli_fetch_assoc($result);
-    return $row['count'];
+    if ($result = mysqli_query($connection, "SELECT count(*) as count FROM photos")) {
+        $row = mysqli_fetch_assoc($result);
+        return $row['count'];
+    } else {
+        logMessage('ERROR','Query error: '. mysqli_error($connection));
+        errorPage();
+    }
 }
 
 function paginate($total, $currentPage, $size) {
